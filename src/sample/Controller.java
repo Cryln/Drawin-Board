3203d316b9e9e15fef8ed3c91637143db62d9a0f
Geralt <superj0529@gmail.com>
2020-12-04 20:49:11 +0800

    Shape.Path 可拖动

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,12 +2,14 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
+import javafx.scene.transform.Translate;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,6 +29,7 @@ public class Controller implements Initializable {
 
 
     private Path currentPath;
+    private Point2D lastP;
 
 
 
@@ -89,13 +92,40 @@ public class Controller implements Initializable {
 
     public void startFreeDraw(Path currentPath,double x,double y){
         MoveTo moveTo = new MoveTo(x,y);
+        System.out.println("panexy:"+drawpane.getLayoutX()+"**"+drawpane.getLayoutY());
+        System.out.println("moveto:"+moveTo.getX()+"**"+moveTo.getY());
         this.currentPath = currentPath;
         //初始化笔刷：
         this.currentPath.getElements().add(moveTo);
         this.currentPath.setStroke(color01.getValue());
         this.currentPath.setStrokeWidth(slider1.getValue());
-        this.currentPath.set;
+        //添加到容器
         drawpane.getChildren().add(this.currentPath);
+        //同时设置一个动作过滤器，来实现移动
+        Translate translate = new Translate(0,0);
+        this.currentPath.getTransforms().add(translate);
+        this.currentPath.addEventFilter(MouseEvent.MOUSE_DRAGGED, e2->{
+            if(e2.getButton()==MouseButton.MIDDLE)
+            {
+
+//                System.out.println("s2l:"+currentP.getX()+"**"+currentP.getY());
+                System.out.println("e2getx:"+e2.getX()+"**"+e2.getY());
+//                System.out.println("e2getscene:"+e2.getSceneX()+"**"+e2.getSceneY());
+                translate.setX(translate.getX()+(e2.getSceneX()-lastP.getX()));
+                translate.setY(translate.getY()+(e2.getSceneY()-lastP.getY()));
+
+//                Translate translate = new Translate(e2.getX()-lastP.getX(),e2.getY()-lastP.getY());
+//                this.currentPath.getTransforms().add(translate);
+
+                lastP = new Point2D(e2.getSceneX(), e2.getSceneY());
+            }
+        });
+        this.currentPath.addEventFilter(MouseEvent.MOUSE_PRESSED, e2->{
+            if(e2.getButton()==MouseButton.MIDDLE)
+            {
+                lastP = new Point2D(e2.getSceneX(), e2.getSceneY());
+            }
+        });
     }
     public void freeDrawing(double x,double y){
         LineTo lineTo = new LineTo(x,y);
